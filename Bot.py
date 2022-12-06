54e43880ad7f6c673ac9acd15785bc5a1c1b1489
Bastian Reuther <bastian@reuther05.de>
2022-12-06 22:49:16 +0100

    reply

--- Bot.py
+++ Bot.py
@@ -10,11 +10,18 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!help'):
+            await message.reply('use google bro', mention_author=True)
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-
-client = MyClient(intents=intents, command_prefix=',', description="W2G Create Room Bot")
+client = MyClient(intents=intents, command_prefix='!', description="W2G Create Room Bot")
 
 client.run(TOKEN)
+
+
